a0d9c328ec363b4f51703b0a1d6c507c742d10f9
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-09 14:15:49 +0530

    Update 38.c

--- 38.c
+++ 38.c
@@ -1,11 +1,11 @@
 #include<stdio.h>           //bitwise operators
 int main()
 {
-    int n1,n2;
+    int n,n1;
     scanf("%d %d",&n1,&n2);
-    n1=n1^n2;                                  // n1=n1+n2;
-    n2=n1^n2;                                   // n2=n1-n2;
-    n1=n1^n2;                                    // n1=n1-n2;
+    n=n^n1;                                  // n=n+n1;
+    n1=n^n1;                                   // n1=n-n1;
+    n=n^n1;                                    // n=n-n1;
     printf("%d %d",n1,n2);
     return 0;
 }
